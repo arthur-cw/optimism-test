5d8f8f7b658c1d9bb4c6427a51b24dc05c44be3f
moreira-a <arthur.moreira@cloudwalk.io>
2021-09-01 01:28:51 -0300

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,59 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@eth-optimism/hardhat-ovm";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      },
+    },
+  },
+  ovm: {
+    solcVersion: '0.7.6+commit.3b061308'
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk'
+      }
+    },
+    eth: {
+      url: 'http://127.0.0.1:9545',
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk'
+      }
+    },
+    optimism: {
+      url: 'http://127.0.0.1:8545',
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk'
+      },
+      gasPrice: 15000000,
+      ovm: true
+    }
+  },
+  etherscan: {
+    // API key for Etherscan
+    apiKey: ""
+  }
+};
---
+++ test/test.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+
+describe("Tester", function() {
+  it("Should test", async function() {
+    const Test = await ethers.getContractFactory("TransferTest");
+    const test = await Test.deploy({ gasLimit: 30000000 });
+    await test.deployed(); 
+
+    [owner, other1, other2] = await ethers.getSigners();
+
+    expect(await test.balance()).to.equal(0);
+    
+    console.log((await owner.getBalance()).toString());
+
+    await owner.sendTransaction({to: test.address, value: 1000000000000});
+
+    console.log((await owner.getBalance()).toString());
+    
+    console.log((await test.balance()).toString())
+    w1 = await test.withdraw1(other1.address, { gasLimit: 30000000 });
+    w2 = await test.withdraw2(other1.address, { gasLimit: 30000000 });
+    await w1.wait();
+    await w2.wait();
+    console.log((await test.balance()).toString());  
+  });
+});
